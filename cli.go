77b70942616b2a5d87ca0c0120f977ea28a835a1
Kawaljit Singh <kawaljit.s@outlook.com>
2022-02-10 19:06:49 -0800

    remove unneeded else ifs

--- cli.go
+++ cli.go
@@ -17,7 +17,7 @@ type Command struct {
 }
 
 // FromStringToCommandStruct takes a line from the user command file as an input and returns a defined golang structure 
-func FromStringToCommandStruct(line string) *Command {
+func FromStringToCommandStruct(line string) (*Command, error) {
 	/* 
 		The line variable value should have a similar format to this: '[1] ADD,oY01WVirLr,63511.53' 
 	*/
@@ -26,30 +26,37 @@ func FromStringToCommandStruct(line string) *Command {
 	cmd := commandVars[0]
 
 	if cmd == "ADD" {
-		return &Command{Command: cmd, Username: commandVars[1], Amount: commandVars[2]}
-	} else if (cmd == "COMMIT_BUY" || cmd == "CANCEL_BUY" || cmd == "COMMIT_SELL" || cmd == "CANCEL_SELL" || cmd == "DISPLAY_SUMMARY") {
-		return &Command{Command: cmd, Username: commandVars[1]}
-	} else if (cmd == "BUY" || cmd == "SELL" || cmd == "SET_BUY_AMOUNT" || cmd == "SET_BUY_TRIGGER" || cmd == "SET_SELL_AMOUNT" || cmd == "SET_SELL_TRIGGER") {
-		return &Command{Command: cmd, Username: commandVars[1], Stock: commandVars[2], Amount: commandVars[3]}
-	} else if (cmd == "QUOTE" || cmd == "CANCEL_SET_BUY" || cmd == "CANCEL_SET_SELL") {
-		return &Command{Command: cmd, Username: commandVars[1], Stock: commandVars[2]}
-	} else if (cmd == "DUMPLOG") {
+		return &Command{Command: cmd, Username: commandVars[1], Amount: commandVars[2]}, nil
+	} 
+	
+	if (cmd == "COMMIT_BUY" || cmd == "CANCEL_BUY" || cmd == "COMMIT_SELL" || cmd == "CANCEL_SELL" || cmd == "DISPLAY_SUMMARY") {
+		return &Command{Command: cmd, Username: commandVars[1]}, nil
+	} 
+	
+	if (cmd == "BUY" || cmd == "SELL" || cmd == "SET_BUY_AMOUNT" || cmd == "SET_BUY_TRIGGER" || cmd == "SET_SELL_AMOUNT" || cmd == "SET_SELL_TRIGGER") {
+		return &Command{Command: cmd, Username: commandVars[1], Stock: commandVars[2], Amount: commandVars[3]}, nil
+	} 
+	
+	if (cmd == "QUOTE" || cmd == "CANCEL_SET_BUY" || cmd == "CANCEL_SET_SELL") {
+		return &Command{Command: cmd, Username: commandVars[1], Stock: commandVars[2]}, nil
+	} 
+	
+	if (cmd == "DUMPLOG") {
 		if len(commandVars) == 3 {
 			// case: DUMPLOG,userid,filename
-			return &Command{Command: cmd, Username: commandVars[1], Filename: commandVars[2]}
+			return &Command{Command: cmd, Username: commandVars[1], Filename: commandVars[2]}, nil
 		} else {
 			// case: DUMPLOG,filename
-			return &Command{Command: cmd, Filename: commandVars[1]}
+			return &Command{Command: cmd, Filename: commandVars[1]}, nil
 		}
-	} else if (cmd == "DISPLAY_SUMMARY") {
-		return &Command{Command: cmd, Username: commandVars[1]}
-	} else {
-		fmt.Printf("Command received: %s, line: %s\n", cmd, line)
-		panic("Unknown command received")
+	} 
+	
+	if (cmd == "DISPLAY_SUMMARY") {
+		return &Command{Command: cmd, Username: commandVars[1]}, nil
 	}
 
+	return nil, fmt.Errorf("unable to conver given line: %s into golang struct", line)
 }
-
 
 func checkError(e error, additionalMessage string) {
 	if e != nil {
@@ -66,7 +73,7 @@ func main() {
 
 	commandsFilePath := os.Args[1]
 	data, err := os.ReadFile(filepath.Clean(commandsFilePath))
-	checkError(err, "Error while reading")
+	checkError(err, "Error while reading file")
 	
 	lines := strings.Split(string(data), "\n")
 	for i, line := range lines {
@@ -74,7 +81,9 @@ func main() {
 			continue
 		}
 
-		requestData := FromStringToCommandStruct(line)
+		requestData, err := FromStringToCommandStruct(line)
+		checkError(err, "")
+
 		fmt.Printf("iteration: %d requestData: %#v\n", i+1, requestData)
 
 		// parsedJson, err := json.Marshal(requestData)
