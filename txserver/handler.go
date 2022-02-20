fc15074db9133b848d3ebfbf527676aeeabcaf55
Kawaljit Singh <kawaljit.s@outlook.com>
2022-02-20 01:43:52 -0800

    handlers return error

--- txserver/handler.go
+++ txserver/handler.go
@@ -16,7 +16,7 @@ const (
 	CONN_PORT = 4444
 )
 
-var handlerMap = map[string]func(*context.Context, *Command){
+var handlerMap = map[string]func(*context.Context, *Command) error {
 	"ADD":              add,
 	"COMMIT_BUY":       commit_buy,
 	"CANCEL_BUY":       cancel_buy,
@@ -35,68 +35,67 @@ var handlerMap = map[string]func(*context.Context, *Command){
 	"DUMPLOG":          dumplog,
 }
 
-func add(ctx *context.Context, command *Command) {
-
+func add(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func commit_buy(ctx *context.Context, command *Command) {
-
+func commit_buy(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func cancel_buy(ctx *context.Context, command *Command) {
-
+func cancel_buy(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func commit_sell(ctx *context.Context, command *Command) {
-
+func commit_sell(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func cancel_sell(ctx *context.Context, command *Command) {
-
+func cancel_sell(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func display_summary(ctx *context.Context, command *Command) {
-
+func display_summary(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func buy(ctx *context.Context, command *Command) {
-
+func buy(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func sell(ctx *context.Context, command *Command) {
-
+func sell(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func set_buy_amount(ctx *context.Context, command *Command) {
-
+func set_buy_amount(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func set_buy_trigger(ctx *context.Context, command *Command) {
-
+func set_buy_trigger(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func set_sell_amount(ctx *context.Context, command *Command) {
-
+func set_sell_amount(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func set_sell_trigger(ctx *context.Context, command *Command) {
-
+func set_sell_trigger(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func quote(ctx *context.Context, command *Command) {
-
+func quote(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func cancel_set_buy(ctx *context.Context, command *Command) {
-
+func cancel_set_buy(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func cancel_set_sell(ctx *context.Context, command *Command) {
-
+func cancel_set_sell(ctx *context.Context, command *Command) error {
+	return nil
 }
 
-func dumplog(ctx *context.Context, command *Command) {
-	eventCollection := mongoClient.Database("test").Collection("events")
+func dumplog(ctx *context.Context, command *Command) error {
 	if command.Username != "" {
 		// get events for specified user
 	} else {
@@ -105,11 +104,16 @@ func dumplog(ctx *context.Context, command *Command) {
 
 
 
+	return nil
 }
 
 func handle(ctx *context.Context, command *Command) {
-	handlerMap[command.Command](ctx, command)
+	err := handlerMap[command.Command](ctx, command)
+	if err != nil {
+		log.Printf("Error handling command %+v, error: %s", command, err)
+	}
 
+	// we should retry if handling fails.
 }
 
 //Use for testing on UVic machine
