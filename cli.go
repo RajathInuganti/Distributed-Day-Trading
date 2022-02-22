7da45d81fbfa4929d6b5e17b35c9e3705bddd451
Kawaljit Singh <kawaljit.s@outlook.com>
2022-02-22 05:54:58 -0800

    parse the errors returned in the response

--- cli.go
+++ cli.go
@@ -23,6 +23,7 @@ type Command struct {
 
 type Response struct {
 	Data []byte `json:"data"`
+	Error string `json:"error"`
 }
 
 // FromStringToCommandStruct takes a line from the user command file as an input and returns a defined golang structure
@@ -69,7 +70,7 @@ func FromStringToCommandStruct(line string) (*Command, error) {
 
 func checkError(e error, additionalMessage string) {
 	if e != nil {
-		fmt.Println(additionalMessage)
+		log.Printf(additionalMessage+": %s\n", e)
 		panic(e)
 	}
 }
@@ -84,38 +85,38 @@ func HandleCommand(command *Command) error {
 
 	res, err := http.Post("http://localhost:8080/", "application/json", &buffer)
 	if err != nil {
-		log.Printf("Error while sending request: %s", err)
+		log.Printf("Error while sending request: %s for command: %+v", err, *command)
 		return err
 	}
 
-	err = HandleResponseForCommand(command, res)
+	err = HandleResponse(command, res)
 	if err != nil {
-		log.Printf("Error while handling response for cmd: %+v: %s", command, err)
+		log.Printf("Error while handling response for cmd: %+v: %s\n", command, err)
 	}
 	return nil
 }
 
-func HandleResponseForCommand(cmd *Command, res *http.Response) error { 
+func HandleResponse(cmd *Command, res *http.Response) error { 
 	log.Printf("Got response: %s for %+v", res.Status, cmd)
 
-	if cmd.Command != "DUMPLOG" && cmd.Command != "DISPLAY_SUMMARY" {
-		// will handle later for other commands
-		return nil
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
+	bodyBytes, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		log.Printf("Error while reading response body: %s\n", err)
 		return err
 	}
 
-	log.Printf("Response body: %s\n", string(body))
+	log.Printf("Response body: %s\n", string(bodyBytes))
 
 	responseStruct := &Response{}
-	err = json.Unmarshal(body, responseStruct)
+	err = json.Unmarshal(bodyBytes, responseStruct)
 	if err != nil { 
 		log.Printf("Error while unmarshalling response body: %s\n", err)
 		return err
+	}
+
+	if responseStruct.Error != "" {
+		log.Printf("Got an error in the response for command: %+v, error: %s\n", cmd, responseStruct.Error)
+		return nil
 	}
 
 	if cmd.Command == "DUMPLOG" {
@@ -128,7 +129,7 @@ func HandleResponseForCommand(cmd *Command, res *http.Response) error {
 		return nil
 	}
 	
-	// For DISPLAY_SUMMARY
+	// For DISPLAY_SUMMARY or other commands
 	fmt.Printf("%s\n", string(responseStruct.Data))
 	return nil
 }
@@ -150,7 +151,7 @@ func main() {
 		}
 
 		requestData, err := FromStringToCommandStruct(line)
-		checkError(err, "")
+		checkError(err, "Couldn't convert line from file to command struct")
 
 		fmt.Printf("iteration: %d requestData: %#v\n", i+1, requestData)
 
@@ -161,7 +162,7 @@ func main() {
 
 		err = HandleCommand(requestData)
 		if err != nil { 
-			log.Printf("Error while handling command %+v: %s", requestData, err) 
+			log.Printf("Error while handling command %+v: %s\n", requestData, err) 
 		}
 	}
 
